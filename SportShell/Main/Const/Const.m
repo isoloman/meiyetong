95a9fad04b35e9e912580c20dd5a42a572b7b449
yingyang <yingyan@xmddkj.com>
2019-06-28 11:50:44 +0800

    1.mdwy

--- SportShell/Main/Const/Const.m
+++ SportShell/Main/Const/Const.m
@@ -1,8 +1,8 @@
 
 
 #import "Const.h"
-static NSString * Protocal = @"http://";//传输协议
-static NSString * BaseUrl = @"yslt-app.meiyetongsoft.com/";
+static NSString * Protocal = @"https://";//传输协议
+static NSString * BaseUrl = @"midaiwuyou-app.meiyetongsoft.com/";
 
 @implementation AppConfig
 +(NSString *)BaseUrl{
